1f5851dcd898ac67221cfb74d64da88bb627d011
Neil Mamparo <neilmamparo@gmail.com>
2026-07-31 14:53:06 -0500

    Added jsHarmony.theme.dark.css

--- jsHarmonyConfig.js
+++ jsHarmonyConfig.js
@@ -274,9 +274,10 @@ function jsHarmonyConfig(config){
   //Theme CSS files
   this.themes = {
     'light': [ 'jsHarmony.theme.light.css' ],
-    'classic': [ 'jsHarmony.theme.classic.css' ]
+    'classic': [ 'jsHarmony.theme.classic.css' ],
+    'dark' : [ 'jsharmony.theme.dark.css']
   };
-  this.theme = 'light';
+  this.theme = 'dark';
   //Additional CSS files for jsHarmony.css
   this.css_extensions = [
     path.dirname(module.filename) + '/public/jquery-ui/css/jquery-ui-1.10.3.custom.min.css',
